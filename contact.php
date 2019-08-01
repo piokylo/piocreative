e8149d68427d3a2056abab404890854732c8bab8
Sean Kiely <seankiely670@gmail.com>
2019-08-01 23:26:45 +0100

    Updates

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST_Post['name'];
+    $visiter_email = $_POST['email']
+    $message = $_POST['message']
+    $business = $_POST['message']
+
+
+    $email_from = 'piocreative.com ' 
+
+    $email_subject = "New Sub"
+
+    $email_body = "User Name: $name.\n";
+                    "User Email: $visiter_email.\n";
+                        "User Message: $message.\n";
+
+    $to = "piocreative.design@gmail.com" 
+    $headers = "From $email_from \r\n";
+    $headers .= "Replay-To:: $visiter_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    
+    header("Location: index.html");
+
+?>
