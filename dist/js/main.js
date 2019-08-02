9f5771c1b691aecf0bd62bfb977a5621aa71e329
Sean Kiely <seankiely670@gmail.com>
2019-08-02 13:50:41 +0100

    added pluse

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,9 @@
-/* it worked! */
+document.querySelector(".circles").animate([
+
+{ transform: 'scale(1)'},
+{ transform: 'scale(1.2)'},
+{ transform: 'scale(1)'}
+], {
+    duration:3000,
+    iteration: Infinity
+});
