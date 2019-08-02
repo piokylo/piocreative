e91a74067eb2b7d1a933fc336ae1e5171314151a
Sean Kiely <seankiely670@gmail.com>
2019-08-02 16:08:19 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,94 @@
-/* it worked! */
+const circles = document.querySelectorAll('.circles')
+
+circles.forEach((circle, index) => {
+    console.log(300 * index)
+    circle.animate(
+        [
+            // keyframes
+            {
+                transform: 'scale(1)'
+            },
+            {
+                transform: 'scale(1.6)'
+            },
+            {
+                transform: 'scale(1)'
+            }
+        ], {
+            // timing options
+            delay: 300 * index,
+            duration: 5000,
+            iterations: Infinity
+        }
+    )
+})
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+const squiggles = document.querySelectorAll('.squiggle')
+
+squiggles.forEach((squiggle, index) => {
+    const randomNumber = random(0, 45)
+
+    console.log(randomNumber)
+
+    squiggle.animate(
+        [
+            // gets a random num between 0 & 45
+            {
+                transform: 'rotate(0)'
+            },
+
+            // { transform: 'rotate(' + randomNumber + 'deg)' },
+            {
+                transform: `rotate(${randomNumber}deg)`
+            },
+
+            {
+                transform: 'rotate(0)'
+            }
+        ], {
+            // timing options
+            delay: 300 * index,
+            duration: 5000,
+            iterations: Infinity
+        }
+    )
+})
+
+
+inView('.container')
+  .on('enter', section => {
+    section.classList.add('in-viewport')
+  })
+  .on('exit', section => {
+    section.classList.remove('in-viewport')
+  })
+
+inView.threshold(0.5)
+
+
+
+const sections = document.querySelectorAll('.section .container')
+
+sections.forEach((section, index) => {
+  // here we use querySelectorAll on our 'section' to only
+  // find elements inside of our section vs. our entire page
+  const artists = section.querySelectorAll('ul.lineup li')
+  const shapes = section.querySelectorAll('.shape')
+
+  artists.forEach((artist, index) => {
+    const delay = index * 100
+    artist.style.transitionDelay = delay + 'ms'
+  })
+
+  shapes.forEach((shape, index) => {
+    // we are setting our delay up to only start once all of our
+    // artists have faded in, using the .length property
+    const delay = (shape.length + index) * 100
+    shape.style.transitionDelay = delay + 'ms'
+  })
+})
